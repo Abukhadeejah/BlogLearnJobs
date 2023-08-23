918302ff2059c645e31267ce4d1665ea2a62bfcb
Abukhadeejah <arsalanahmed82@hotmail.com>
2023-08-23 19:49:36 +0530

    views built

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    # Get the list of posts
+    posts = Post.published.all()
+    return render(request, 
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+def post_details(request, id):
+    # Get the details of a post by its id
+    post = get_object_or_404(Post, 
+                             id=id,
+                             status=Post.Status.PUBLISHED)
+    
+    return render(request, 
+                  'blog/post/details.html',
+                  {'post': post})
+    
