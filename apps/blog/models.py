06ac8dfbb19a0d80a6a1a3378f59c23f1dedc990
Abukhadeejah <arsalanahmed82@hotmail.com>
2023-08-23 11:29:46 +0530

    db indexing and default sorting added.Draft and Published status for blog added.

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -3,12 +3,29 @@ from django.utils import timezone
 
 class Post(models.Model):
     """ Post model to allow storage of blog posts in db. """
+
+    class Status(models.TextChoice):
+        """ subclass to allow writers to save blog posts as draft or published status. """
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now())
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    class Meta:
+        """ Posts will be returned in reverse chronological order by default"""
+        ordering = ['-publish']
+        indexes = [
+            models.IndexField(fields=['-publish']),
+        ]
 
     def __str__(self) -> str:
         return self.title
